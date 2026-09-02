1f5aef4ce73544d343c4a4d79f6010064ca5c19a
Kasper Ploug Jepsen <jepsenkasper@gmail.com>
2026-09-02 16:39:34 +0200

    Bump app version to 1.1.0

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: appName,
     slug: 'flamme-rouge-grand-tour',
-    version: '1.0.0',
+    version: '1.1.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'flammerougegrandtour',
